3fa4a5f42e53eba2b5e1dfb180e2581859508fcc
pasDamola <oyindadavid18@gmail.com>
2023-08-30 01:48:58 +0100

    creating passwords through a join table

--- app/models/password.rb
+++ app/models/password.rb
@@ -1,4 +1,11 @@
 class Password < ApplicationRecord
     has_many :user_passwords
     has_many :users, through: :user_passwords
+
+    encrypts :username, deterministic: true
+    encrypts :password
+
+    validates :username, presence: true
+    validates :url, presence: true
+    validates :password, presence: true
 end
